d5d478b7205b8576349b81bb83be82f8c1f03158
Max W. Y. Lam <maxingaussian@gmail.com>
2016-10-05 10:32:32 +0800

    latest commit

--- SCFGP/SCFGP.py
+++ SCFGP/SCFGP.py
@@ -291,7 +291,6 @@ class SCFGP(object):
         with open(path, "rb") as load_f:
             load_pack = pickle.load(load_f)
         self.ID, self.seed, self.R, self.M = load_pack[0]
-        npr.seed(self.seed)
         self.NAME = "SCFGP (Rank=%s, Feature Size=%d)"%(str(self.R), self.M)
         self.X_nml, self.y_nml, self.efd = load_pack[1]
         self.X, self.y = load_pack[2]
