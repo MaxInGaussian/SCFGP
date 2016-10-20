902a689ca758499961735adb6d4652bd31755af0
Max W. Y. Lam <maxingaussian@gmail.com>
2016-10-21 02:07:17 +0800

    lastest commit

--- SCFGP/Scaler.py
+++ SCFGP/Scaler.py
@@ -67,7 +67,7 @@ class Scaler(object):
                 ks_func = lambda x: kstest(x.ravel(), 'norm')[0]
                 fun = lambda b: ks_func(boxcox(tX[:, d]+b[0]**2, lmb_func(b)))
                 b = minimize(fun, [.3], method='SLSQP', bounds=[(0.01, 2)],
-                    options={'xtol': 1e-4, 'maxiter':100, 'disp': True})['x']
+                    options={'rtol': 1e-4, 'maxiter':100, 'disp': True})['x']
                 self.data['bias'][d] = b[0]**2
                 self.data['lmb'][d] = boxcox_normmax(tX[:, d]+b[0]**2)
             tX += self.data['bias'][None, :]
